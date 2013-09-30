d21bf381a77b0c4c466a474b061169946bf354b2
Forbes Lindesay <forbes.lindesay@red-gate.com>
2013-09-30 15:31:05 +0100

    Clean up styles

--- app.js
+++ app.js
@@ -1,131 +1,122 @@
-(function app($) {
-    $(function app() {
-        var empty = 0;
-        var nought = 1;
-        var cross = 2;
 
-        var currentGameState;
-        //var currentPlayer = nought;
+var empty = 0;
+var nought = 1;
+var cross = 2;
 
-        $('#game-board td').click(onClick);
+var currentGameState;
+//var currentPlayer = nought;
 
-        function onClick() {
-            //alert(getColumn(this) + getRow(this));
-            var x = getColumn(this);
-            var y = getRow(this);
-            console.log(x + ', ' + y);
-            if (currentGameState[y][x] === empty) {
-                currentGameState[y][x] = nought;
-                $(this).html('o').addClass('player-o').removeClass('empty');
+$('#game-board button').click(onClick);
+
+function onClick() {
+    //alert(getColumn(this) + getRow(this));
+    var x = getColumn(this);
+    var y = getRow(this);
+    if (currentGameState[y][x] === empty) {
+        currentGameState[y][x] = nought;
+        renderGame();
+
+        setTimeout(function () {
+            checkWinner();
+            checkDraw();
+
+            playX();
+
+            setTimeout(function () {
                 checkWinner();
                 checkDraw();
-                
-                playX();               
-                checkWinner();
-                checkDraw();
-            }
-        }
-        
-        function playX () {
-            var rand1;
-            var rand2;
-            var stop = false;
-            while (stop === false) {
-                rand1 = getRandom(0, 2);
-                rand2 = getRandom(0, 2);
-                console.log(rand1 + ', ' + rand2);
-                if (currentGameState[rand2][rand1] === empty) {
-                    currentGameState[rand2][rand1] = cross;
-                    
-                    if (rand2 === 0) {
-                        if (rand1 === 0) {
-                            $( '#one' ).html('x').addClass('player-x').removeClass('empty');
-                        }
-                        else if (rand1 === 1) {
-                            $( '#two' ).html('x').addClass('player-x').removeClass('empty');
-                        }
-                        else if (rand1 === 2) {
-                            $( '#three' ).html('x').addClass('player-x').removeClass('empty');
-                        }
-                    } else if (rand2 === 1) {
-                        if (rand1 === 0) {
-                            $( '#four' ).html('x').addClass('player-x').removeClass('empty');
-                        }
-                        else if (rand1 === 1) {
-                            $( '#five' ).html('x').addClass('player-x').removeClass('empty');
-                        }
-                        else if (rand1 === 2) {
-                            $( '#six' ).html('x').addClass('player-x').removeClass('empty');
-                        } 
-                    } else if (rand2 === 2) {
-                        if (rand1 === 0) {
-                            $( '#seven' ).html('x').addClass('player-x').removeClass('empty');
-                        }
-                        else if (rand1 === 1) {
-                            $( '#eight' ).html('x').addClass('player-x').removeClass('empty');
-                        }
-                        else if (rand1 === 2) {
-                            $( '#nine' ).html('x').addClass('player-x').removeClass('empty');
-                        }
-                    }
-                    stop = true;
-                }
-            }
-        }
-            
-        function getRandom (min, max) {
-                return Math.floor(Math.random() * (max - min + 1) + min);
-        }
+            }, 100);
+        }, 100);
+    }
+}
 
-        function getRow(cell) {
-            return $(cell).attr('data-loc').charAt(2);
+function playX () {
+    while (true) {
+        var x = getRandom(0, 2);
+        var y = getRandom(0, 2);
+        console.log(x + ', ' + y);
+        if (currentGameState[y][x] === empty) {
+            currentGameState[y][x] = cross;
+            renderGame();
+            break;
         }
-        function getColumn(cell) {
-            return $(cell).attr('data-loc').charAt(0);
-        }
-        function checkWinner() {
-            checkGroup([0,0], [1,0], [2,0]);
-            checkGroup([0,1], [1,1], [2,1]);
-            checkGroup([0,2], [1,2], [2,2]);
-            checkGroup([0,0], [0,1], [0,2]);
-            checkGroup([1,0], [1,1], [1,2]);
-            checkGroup([2,0], [2,1], [2,2]);
-            checkGroup([0,0], [1,1], [2,2]);
-            checkGroup([0,2], [1,1], [2,0]);
-        }
-        function checkGroup(p1, p2, p3) {
-            if (state(p1) === state(p2) && state(p2) === state(p3) && state(p1) !== empty) {
-                if (state(p1) === nought) {
-                    alert('Nought wins!');
-                } else {
-                    alert('Cross wins!');
-                }
-                resetGame();
-            }
-        }
-        function state(point) {
-            var x = point[0];
-            var y = point[1];
-            return currentGameState[y][x];
-        }
-        function checkDraw() {
-            for (var x = 0; x < 3; x++) {
-                for (var y = 0; y < 3; y++) {
-                    if (state([x,y]) === empty) {
-                        return;
-                    }
-                }
-            }
-            alert('It\'s a draw');
-            resetGame();
-        }
-        function resetGame() {
-            currentGameState = [
-            [empty, empty, empty],
-            [empty, empty, empty],
-            [empty, empty, empty]];
-            $('#game-board td').html('').addClass('empty').removeClass('player-x').removeClass('player-o');
+    }
+}
+
+function getRandom (min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRow(cell) {
+    return $(cell).attr('data-loc').charAt(2);
+}
+function getColumn(cell) {
+    return $(cell).attr('data-loc').charAt(0);
+}
+function checkWinner() {
+    checkGroup([0,0], [1,0], [2,0]);
+    checkGroup([0,1], [1,1], [2,1]);
+    checkGroup([0,2], [1,2], [2,2]);
+    checkGroup([0,0], [0,1], [0,2]);
+    checkGroup([1,0], [1,1], [1,2]);
+    checkGroup([2,0], [2,1], [2,2]);
+    checkGroup([0,0], [1,1], [2,2]);
+    checkGroup([0,2], [1,1], [2,0]);
+}
+function checkGroup(p1, p2, p3) {
+    if (state(p1) === state(p2) && state(p2) === state(p3) && state(p1) !== empty) {
+        if (state(p1) === nought) {
+            alert('Nought wins!');
+        } else {
+            alert('Cross wins!');
         }
         resetGame();
-    });
-}(jQuery));
+    }
+}
+function state(point) {
+    var x = point[0];
+    var y = point[1];
+    return currentGameState[y][x];
+}
+function checkDraw() {
+    for (var x = 0; x < 3; x++) {
+        for (var y = 0; y < 3; y++) {
+            if (state([x, y]) === empty) {
+                return;
+            }
+        }
+    }
+    alert('It\'s a draw');
+    resetGame();
+}
+function resetGame() {
+    currentGameState = [
+    [empty, empty, empty],
+    [empty, empty, empty],
+    [empty, empty, empty]];
+    renderGame();
+}
+
+function renderGame() {
+    for (var y = 0; y < 3; y++) {
+        for (var x = 0; x < 3; x++) {
+            if (currentGameState[y][x] === empty)
+                $('[data-loc="' + x + ',' + y + '"]').html('').removeClass('player-x').removeClass('player-o').addClass('empty')
+            else if (currentGameState[y][x] === nought)
+                $('[data-loc="' + x + ',' + y + '"]').html('o').removeClass('player-x').addClass('player-o').removeClass('empty')
+            else if (currentGameState[y][x] === cross)
+                $('[data-loc="' + x + ',' + y + '"]').html('x').addClass('player-x').removeClass('player-o').removeClass('empty')
+        }
+    }
+}
+resetGame();
+
+
+
+$(window).resize(resizeCells);
+resizeCells();
+function resizeCells() {
+    //resize font and height to match width
+    var cw = $('#game-board td').width();
+    $('#game-board td').css({'height':cw+'px', 'font-size': (cw / 1.5) + 'px'});
+}
